d9d4a04ab0a622f0a7fbada9eeb8e1d466bab956
Abdullahi Mujaheed <114176272+Abdul10gits@users.noreply.github.com>
2025-10-27 03:07:13 +0100

    fastapi deployment homework5

---
+++ module5_test.py
@@ -0,0 +1,21 @@
+import pickle as pkl
+from sklearn.linear_model import LogisticRegression
+from sklearn.feature_extraction import DictVectorizer
+import numpy as np
+from fastapi import FastAPI, Request
+from pydantic import BaseModel
+
+app = FastAPI()
+
+with open(r'C:\Users\Abdullahi Mujaheed\Desktop\mlzoom\mlzoomcamp\models.bin', 'rb') as f_out:
+    dv, model = pkl.load(f_out)
+
+class InputData(BaseModel):
+    lead_source: str
+    number_of_courses_viewed: int
+    annual_income: float
+@app.post('/mod5_deployment')
+def trans_predict(input_data: InputData):
+    client = input_data.dict()
+    prediction = model.predict_proba(dv.transform(client))
+    return prediction
